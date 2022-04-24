979504da84abde22cb1db69fe477ca87ddcca63f
mohammad khodabakhsh <mohammadkhodabakhsh82@gmail.com>
2022-04-24 12:45:15 +0430

    add user store contract

---
+++ contract/user_store.go
@@ -0,0 +1,14 @@
+package contract
+
+import (
+	"context"
+
+	"gitlab.com/gocastsian/writino/entity"
+)
+
+type UserStore interface {
+	Create(context.Context, entity.User) (entity.User, error)
+	FindbyID(context.Context, string) (entity.User, error)
+	Update(context.Context, entity.User) (entity.User, error)
+	Delete(context.Context, string) error
+}
