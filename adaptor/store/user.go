16fd29abbe77ec2a294fe72da2fad8ec322c78de
mohammad khodabakhsh <mohammadkhodabakhsh82@gmail.com>
2022-04-26 09:34:43 +0430

    add user crud

---
+++ adaptor/store/user.go
@@ -0,0 +1,59 @@
+package store
+
+import (
+	"context"
+
+	"gitlab.com/gocastsian/writino/entity"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+func (m MongodbStore) Create(ctx context.Context, user entity.User) (entity.User, error) {
+
+	coll := m.db.Collection("users")
+
+	user.Id = primitive.NewObjectID()
+	res, err := coll.InsertOne(ctx, user)
+	if err != nil {
+		return entity.User{}, err
+	}
+	user.Id = res.InsertedID.(primitive.ObjectID)
+	return user, nil
+}
+
+func (m MongodbStore) FindbyID(ctx context.Context, id string) (entity.User, error) {
+
+	var user entity.User
+	coll := m.db.Collection("users")
+
+	objID, err := primitive.ObjectIDFromHex(id)
+	if err != nil {
+		return user, err
+	}
+	filter := bson.D{{"_id", objID}}
+	res := coll.FindOne(ctx, filter, nil)
+	err = res.Decode(&user)
+	return user, err
+}
+
+func (m MongodbStore) Update(ctx context.Context, user entity.User) error {
+
+	coll := m.db.Collection("users")
+
+	filter := bson.D{{"_id", user.Id}}
+	_, err := coll.ReplaceOne(ctx, filter, user, nil)
+	return err
+}
+
+func (m MongodbStore) Delete(ctx context.Context, id string) error {
+
+	coll := m.db.Collection("users")
+
+	objID, err := primitive.ObjectIDFromHex(id)
+	if err != nil {
+		return err
+	}
+	filter := bson.D{{"_id", objID}}
+	_, err = coll.DeleteOne(ctx, filter, nil)
+	return err
+}
--- contract/user_store.go
+++ contract/user_store.go
@@ -9,6 +9,6 @@ import (
 type UserStore interface {
 	Create(context.Context, entity.User) (entity.User, error)
 	FindbyID(context.Context, string) (entity.User, error)
-	Update(context.Context, entity.User) (entity.User, error)
+	Update(context.Context, entity.User) error
 	Delete(context.Context, string) error
 }
