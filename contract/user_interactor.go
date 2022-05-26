1cbac55d14ab742cd95971bdc007ea20f321eb59
mohammad khodabakhsh <mohammadkhodabakhsh82@gmail.com>
2022-05-26 23:04:45 +0430

    move validation to delivery layer

--- contract/user_interactor.go
+++ contract/user_interactor.go
@@ -9,9 +9,9 @@ import (
 type UserInteractor interface {
 	CheckUsername(context.Context, dto.CheckUsernameReq) (dto.CheckUsernameRes, error)
 	CheckEmail(context.Context, dto.CheckEmailReq) (dto.CheckEmailRes, error)
-	Register(context.Context, dto.RegisterReq, ValidateRegisterUser) error
-	Update(context.Context, dto.UpdateUserReq, ValidateUpdateUser) error
-	UpdatePassword(context.Context, dto.UpdatePasswordReq, ValidateUpdatePassword) error
+	Register(context.Context, dto.RegisterReq) error
+	Update(context.Context, dto.UpdateUserReq) error
+	UpdatePassword(context.Context, dto.UpdatePasswordReq) error
 	Find(context.Context, dto.FindUserReq) (dto.FindUserRes, error)
 	DeleteAccount(context.Context, dto.DeleteUserReq) error
 	VerifyUser(context.Context, dto.VerifyUserReq) error
